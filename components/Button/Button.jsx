d23a9cbae0f85e61e56c301270c076b15b5e352b
Gabriela Rodrigues <57119264+gabrdgs@users.noreply.github.com>
2021-09-09 16:19:29 -0300

    Intrgration SignUp

--- components/Button/Button.jsx
+++ components/Button/Button.jsx
@@ -2,7 +2,7 @@
 export default function Button({icon = false, dark = false, children}) {
   return (
     <div className={dark ? 'hero-btn-l6' : 'l1-create-acc-btn'}>
-      <a href="#" className="btn btn-style-02">
+      <a href="" className="btn btn-style-02">
         {children}
 
         {icon && <i className="fas fa-angle-right" />}
