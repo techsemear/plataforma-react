1512177381500769b9cccad92c3bc1ef215bb2b7
Gabriela Rodrigues <57119264+gabrdgs@users.noreply.github.com>
2021-09-10 17:48:45 -0300

    Add Title Web Page

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,13 @@ import {Presentation} from '../containers/Presentation'
 import {StudentArea} from '../containers/StudentArea'
 import {Subscribe} from '../containers/Subscribe'
 
+// Components
+import { IndexPage } from '../components/IndexPage'
+
 export default function Home() {
   return (
     <React.Fragment>
+      <IndexPage/>
       <Navbar />
       <Hero />
       <Presentation />
