8837622ae5b39b55f84143d46d6d5c9d852723db
Gabriela Rodrigues <57119264+gabrdgs@users.noreply.github.com>
2021-09-20 11:53:27 -0300

    Update Image and Css

--- containers/MaslowPoints/MaslowPoints.jsx
+++ containers/MaslowPoints/MaslowPoints.jsx
@@ -6,7 +6,7 @@ import {Heading} from '../../components/Heading'
 import {Section} from '../../components/Section'
 import {MaslowFeatures} from '../../components/MaslowFeatures'
 
-import maslowPiramid from '../../assets/image/maslow.png'
+import maslowPiramid from '../../assets/image/triangulo-blue.png'
 
 
 
@@ -15,7 +15,6 @@ export default function MaslowPoints() {
     <Fragment>
     <Section bgColor="primary">
     <div className="content-area-l7-2">
-    <div className="container">
     <div className="row content-area-l7-2-heading align-items-center justify-content-lg-start justify-content-center">
       <div className="col-lg-6 col-md-7">
         <div className="section__heading-3">
@@ -24,7 +23,7 @@ export default function MaslowPoints() {
       </div>
     </div>
     <div className="row align-items-center justify-content-lg-start justify-content-center">
-      <div className="col-xxl-5 col-xl-6 col-lg-6 col-md-10 order-lg-1 order-1">
+      <div className="col-xl-4 col-lg-6 col-md-10 order-lg-1 order-1">
         <div className="content-l7-2-content-box">
           <Heading level="h6" ftColor="primary">Ela categoriza as principais necessidades humanas em um gráfico de pirâmide! Segue com a gente:</Heading>
           <div className="item d-flex flex-wrap " data-aos="fade-up" data-aos-delay={500} data-aos-duration={1000}>
@@ -56,14 +55,13 @@ export default function MaslowPoints() {
           </div>
         </div>
       </div>
-      <div className="offset-xxl-1 col-xxl-6 col-xl-6 col-lg-6 col-md-10 order-lg-1 order-0" data-aos="fade-up" data-aos-delay={500} data-aos-duration={1000}>
+      <div className="col-xxl-5 col-xl-6 col-lg-6 col-md-10 order-lg-1 order-0" data-aos="fade-up" data-aos-delay={500} data-aos-duration={1000}>
         <div className="content-2-l7-image overflow-hidden image-hover-style-01">
           <Image className="w-100" src={maslowPiramid} alt="image" />
         </div>
       </div>
+    </div> 
     </div>
-    </div>
-    </div>    
   </Section>
   <MaslowFeatures/>
   </Fragment>
