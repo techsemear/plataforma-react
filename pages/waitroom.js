6b74cc1288576326c753a283fa3f256776ae19b9
Jéssica Gonsalves <82064258+jessicagonsalves@users.noreply.github.com>
2021-09-22 18:23:56 -0300

    Create waitroom.js

---
+++ pages/waitroom.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// Components
+
+// Containers
+import {ChallengeVideos} from '../containers/ChallengeVideos'
+
+export default function Onboarding() {
+  return (
+    <React.Fragment>
+      <ChallengeVideos />
+    </React.Fragment>
+  )
+}
