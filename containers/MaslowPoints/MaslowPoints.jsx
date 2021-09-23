1557e2f4fccf12c79c89df8c00c4e4d87d1cc4dc
Gabriela Rodrigues <57119264+gabrdgs@users.noreply.github.com>
2021-09-23 15:45:01 -0300

    Add variables on MaslowPoints

--- containers/MaslowPoints/MaslowPoints.jsx
+++ containers/MaslowPoints/MaslowPoints.jsx
@@ -7,6 +7,24 @@ import {Section} from '../../components/Section'
 import maslowPiramid from '../../assets/image/triangulo-blue.png'
 
 
+const contentText = {
+  title: 'Seguimos um conceito científico aqui no Instituto chamado "Pirâmide de Maslow"',
+  subtitle: 'Abraham Maslow, o pai da Psicologia Humanista, é o autor do livro A Teoria da Motivação Humana, no qual ele discorre sobre os princípios da motivação e hierarquiza as necessidades humanas em forma de pirâmide.',
+}
+
+
+const listText = {
+  title: [
+    '1° Nível',
+    '2° Nível',
+    '3° Nível',
+  ],
+  subtitle: [
+    'Desafios de Base',
+    'Desafios Psicológicos',
+    'Desafios de Autorrealização',
+  ]
+}
 
 export default function MaslowPoints() {
   return (
@@ -16,9 +34,13 @@ export default function MaslowPoints() {
     <div className="row content-area-l7-2-heading align-items-center justify-content-lg-start justify-content-center">
       <div className="col-lg-9 col-md-7">
         <div className="section__heading-3">
-          <Heading level="h4" horizontalPosition="center" ftColor="primary">Seguimos um conceito científico aqui no Instituto chamado 'Pirâmide de Maslow</Heading>
+          <Heading level="h4" horizontalPosition="center" ftColor="primary">
+            {contentText.title}
+          </Heading>
           <br/>
-          <Heading level="h6" horizontalPosition="center" ftColor="primary">Abraham Maslow, o pai da Psicologia Humanista, é o autor do livro A Teoria da Motivação Humana, no qual ele discorre sobre os princípios da motivação e hierarquiza as necessidades humanas em forma de pirâmide.</Heading>
+          <Heading level="h6" horizontalPosition="center" ftColor="primary">
+            {contentText.subtitle}
+          </Heading>
         </div>
       </div>
     </div>
@@ -30,8 +52,12 @@ export default function MaslowPoints() {
               <div className="d-flex">
                 <div className="color-box bg-grey1" />
                 <div className="content-box">
-                  <Heading level ="h4" ftColor="primary">3° Nível</Heading>
-                  <Heading level ="h6" ftColor="primary">Desafios de Autorrealização</Heading>
+                  <Heading level ="h4" ftColor="primary">
+                    {listText.title[2]}
+                  </Heading>
+                  <Heading level ="h6" ftColor="primary">
+                    {listText.subtitle[2]}
+                  </Heading>
                 </div>
               </div>
             </div>
@@ -39,8 +65,12 @@ export default function MaslowPoints() {
               <div className="d-flex">
                 <div className="color-box bg-mint" />
                 <div className="content-box">
-                  <Heading level ="h4" ftColor="primary">2° Nível</Heading>
-                  <Heading level ="h6" ftColor="primary">Desafios Psicológicos    </Heading>
+                  <Heading level ="h4" ftColor="primary">
+                    {listText.title[1]}
+                  </Heading>
+                  <Heading level ="h6" ftColor="primary">
+                    {listText.subtitle[1]}
+                  </Heading>
                 </div>
               </div>
             </div>
@@ -48,8 +78,12 @@ export default function MaslowPoints() {
               <div className="d-flex">
                 <div className="color-box bg-secondBlue" />
                 <div className="content-box">
-                  <Heading level ="h4" ftColor="primary">1° Nível</Heading>
-                  <Heading level ="h6" ftColor="primary">Desafios de Base</Heading>
+                  <Heading level ="h4" ftColor="primary">
+                    {listText.title[0]}
+                  </Heading>
+                  <Heading level ="h6" ftColor="primary">
+                    {listText.subtitle[0]}
+                  </Heading>
                 </div>
               </div>
             </div>
