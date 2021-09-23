f1102f381dabc44e2ba8720e978aea168697d20c
Gabriela Rodrigues <57119264+gabrdgs@users.noreply.github.com>
2021-09-23 17:07:20 -0300

    Update SlideShow

--- components/SlideShow/SlideShow.jsx
+++ components/SlideShow/SlideShow.jsx
@@ -7,11 +7,6 @@ import imageLogin1 from '../../assets/image/loginImage1.png'
 import imageLogin2 from '../../assets/image/loginImage2.png'
 import imageLogin3 from '../../assets/image/loginImage3.png'
 
-const fadeImages = {
-  image1: '../../assets/image/loginImage1.png',
-  image2: '../../assets/image/loginImage2.png',
-  image3: '../../assets/image/loginImage3.png',
-}
 
 const SlideShow = () => {
   return (
@@ -19,21 +14,30 @@ const SlideShow = () => {
       <Fade>
         <div className="each-fade">
           <div className="image-container">
-            <Image src= {imageLogin1} />
+            <Image 
+              src= {imageLogin1}
+              width={550}
+              height={550}
+            />
           </div>
-          <h2>First Slide</h2>
         </div>
         <div className="each-fade">
           <div className="image-container">
-            <Image src={imageLogin2} />
+            <Image 
+              src={imageLogin2}
+              width={550}
+              height={550}
+            />
           </div>
-          <h2>Second Slide</h2>
         </div>
         <div className="each-fade">
           <div className="image-container">
-            <Image src={imageLogin3} />
+            <Image 
+              src={imageLogin3}
+              width={550}
+              height={550}
+            />
           </div>
-          <h2>Third Slide</h2>
         </div>
       </Fade>
     </div>
--- containers/LoginPage/LoginPage.jsx
+++ containers/LoginPage/LoginPage.jsx
@@ -15,7 +15,6 @@ import imageLogin3 from '../../assets/image/loginImage3.png'
 
 
 
-
 export default function LoginPage(){
   return (
     <main>
@@ -24,7 +23,11 @@ export default function LoginPage(){
         <div className="forms-wrap">
           <form action="index.html" autoComplete="off" className="sign-in-form">
             <div className="plataforma-logo">
-              <Image src = {plataformaLogo}/>
+              <Image
+                src = {plataformaLogo}
+                width={550}
+                height={550}
+              />
             </div>
             <div className="heading">
               <Heading level="h2">Bem-vindo</Heading>
@@ -50,9 +53,12 @@ export default function LoginPage(){
                       Facebook
                     </a>
                     <a href="#" className="btn-google">
-                      <Image src={googleLogo} alt="GOOGLE" />
+                      <Image 
+                      src={googleLogo} 
+                      alt="GOOGLE" 
+                      />
                       Google
-                    </a>
+                      </a>
                 </div>
               </div>
               <p className="text">
@@ -61,59 +67,13 @@ export default function LoginPage(){
               </p>
             </div>
           </form>
-          <form action="index.html" autoComplete="off" className="sign-up-form">
-            <div className="logo">
-              <h4>FernandaMakiHirose</h4>
-            </div>
-            <div className="heading">
-              <h2>Comece sua jornada</h2>
-              <h6>Já tem uma conta?</h6>
-              <a href="#" className="toggle">Entre aqui</a>
-            </div>
-            <div className="actual-form">
-              <div className="input-wrap">
-                <input type="text" minLength={4} className="input-field" autoComplete="off" required />
-                <label>Nome</label>
-              </div>
-              <div className="input-wrap">
-                <input type="email" className="input-field" autoComplete="off" required />
-                <label>E-mail</label>
-              </div>
-              <div className="input-wrap">
-                <input type="password" minLength={4} className="input-field" autoComplete="off" required />
-                <label>Senha</label>
-              </div>
-              <input type="submit" defaultValue="Sign Up" className="sign-btn" />
-              <p className="text">
-                Após entrar, eu concordo com os
-                <a href="#">Termos e Serviços</a> e com a
-                <a href="#">Política de Privacidade</a>
-              </p>
-            </div>
-          </form>
         </div>
-        <div className = "carrossel" >
-          <div className = "images-wrapper" >
-            <SlideShow/>
-          </div>
-          <div className="text-slider">
-            <div className="text-wrap">
-              <div className="text-group">
-                <h2>Cursos gratuitos</h2>
-                <h2>Suporte ao vivo</h2>
-                <h2>Aprenda em casa</h2>
-              </div>
-            </div>
-            <div className="bullets">
-              <span className="active" data-value={1} />
-              <span data-value={2} />
-              <span data-value={3} />
-            </div>
-          </div>
+        <div className="slider">
+          <SlideShow/>
+          <Heading className="slider-text" level="h3" ftColor = "default" horizontalPosition="center" >Faça parte dessa jornada !</Heading>
+        </div>
         </div>
       </div>
-    </div>
-  </main>
-  
+    </main>
   )
 }
