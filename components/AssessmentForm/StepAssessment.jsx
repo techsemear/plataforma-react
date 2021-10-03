332ce6c1ace7b619e517df769c5c4574e5e485ee
Gabriela Rodrigues <57119264+gabrdgs@users.noreply.github.com>
2021-10-03 20:14:44 -0300

    update range Assessment

--- components/AssessmentForm/StepAssessment.jsx
+++ components/AssessmentForm/StepAssessment.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import {render} from 'react-dom'
+import React, {Fragment, useState} from 'react'
 import Image from 'next/image'
-import { Fragment } from 'react'
 
 import {Heading} from '..//Heading'
 
-import { loadGetInitialProps } from 'next/dist/shared/lib/utils'
+import sad from '../../assets/image/1.png'
+import halfSad from '../../assets/image/2.png'
+import half from '../../assets/image/3.png'
+import halfHappy from '../../assets/image/4.png'
+import happy from '../../assets/image/5.png'
+
+import {loadGetInitialProps} from 'next/dist/shared/lib/utils'
 
 let StepAssessment = (props) => {
   if (props.currentStep !== props.stepStatus) {
@@ -24,11 +28,7 @@ let StepAssessment = (props) => {
               data-aos-once="true"
             >
               <div className="content position-text">
-                <Heading
-                  level="h1"
-                  ftColor="quinary"
-                  className="fs-title"
-                >
+                <Heading level="h1" ftColor="quinary" className="fs-title">
                   {props.content.title}
                 </Heading>
                 <Heading level="h5">{props.content.subtitle}</Heading>
@@ -43,98 +43,117 @@ let StepAssessment = (props) => {
               data-aos-once="true"
             >
               <div className="hero-video-l12 position-relative">
-                <Image src={props.content.gif} alt="gif" width={400} height={300} />
+                <Image
+                  src={props.content.gif}
+                  alt="gif"
+                  width={400}
+                  height={300}
+                />
               </div>
             </div>
           </div>
         </div>
       </div>
-      <Range props={props}/>
-      <RangeApp />
+      <Range props={props} />
     </fieldset>
   )
 }
 
 export default StepAssessment
 
-function Range(props) {
+const assessmentStatus = {
+  0: {
+    icon: <Image src={sad}/>,
+    label: "",
+  },
+  1: {
+    icon: <Image src={sad}/>,
+    label: "",
+  },
+  2: {
+    icon: <Image src={halfSad}/>,
+    label: "",
+  },
+  3: {
+    icon: <Image src={halfSad}/>,
+    label: "",
+  },
+  4: {
+    icon: <Image src={halfSad}/>,
+    label: "",
+  },
+  5: {
+    icon: <Image src={half}/>,
+    label: "",
+  },
+  6: {
+    icon: <Image src={half}/>,
+    label: "",
+  },
+  7: {
+    icon: <Image src={halfHappy}/>,
+    label: "",
+  },
+  8: {
+    icon: <Image src={happy}/>,
+    label: "",
+  },
+  9: {
+    icon: <Image src={happy}/>,
+    label: "",
+  },
+  10: {
+    icon: <Image src={happy}/>,
+    label: "",
+  },
+}
+
+function Range() {
+  const [inputAssessment, setInputAssessment] = useState(7)
+
+  const handleChange = (event) => {
+    const value = event.currentTarget.value
+    setInputAssessment(value)
+  }
+  const getStatusClassName = () => {
+      if (inputAssessment >= 0 && inputAssessment < 4)  
+      {
+        return "rangeStatus--danger"
+      }
+      if(inputAssessment > 3 && inputAssessment < 7)  
+      {
+        return "rangeStatus--warning"
+      }
+      if(inputAssessment == 7)  
+      {
+        return "rangeStatus--neutro"
+      }
+      if(inputAssessment > 7)  
+      {
+        return "rangeStatus--success"
+      }
+    }
   return (
     <Fragment>
-      <input
-        type="range"
-        min="0"
-        max="10"
-        onInput="this.nextElementSibling.value = this.value"
-        list="tickmarks"
-        className="range"
-        placeholder="Qual seu nível de escolaridade"
-        value={props.rating}
-        onChange={props.handleChange}
-        required
-      />
-      <datalist id="tickmarks">
-        <option value={0} label="0%"></option>
-        <option value={10}></option>
-        <option value={20}></option>
-        <option value={30}></option>
-        <option value={40}></option>
-        <option value={50} label="50%"></option>
-        <option value={60}></option>
-        <option value={70}></option>
-        <option value={80}></option>
-        <option value={90}></option>
-        <option value={100} label="100%"></option>
-      </datalist>
-      <output></output>
+      <div className="rangeAssessmentStatus">
+        <span>{assessmentStatus[inputAssessment].icon}</span>
+
+        <span className={`teste ${getStatusClassName()}`}>{assessmentStatus[inputAssessment].label}</span>
+      </div>
+
+      <div className="rangeAssessment">
+        <input
+          className={`rangeAssessmentInput ${getStatusClassName()}`}
+          type="range"
+          min="0"
+          max="10"
+          step="1"
+          value={inputAssessment}
+          onChange={handleChange}
+          required
+        />
+        <span>{inputAssessment}</span>
+      </div>
     </Fragment>
   )
 }
-
-class RangeApp extends React.Component {
-  values = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-  constructor(props) {
-    super(props)
-    this.state = {
-      currentStepIndex: 0,
-    }
-  }
-
-  handleInputChange = (e) => {
-    this.setState({currentStepIndex: e.currentTarget.value})
-  }
-
-  render() {
-    return (
-      <div className="col-xl-8 col-lg-9 col-md-10 col-sm-12 order-lg-1 order-1 block">
-        <input
-          onInput={this.handleInputChange}
-          type="range"
-          min="0"
-          value={this.state.currentStepIndex}
-          max="10"
-          step="1"
-          list="tick-list"
-        />
-        <datalist id="tick-list">
-          <option value={0} label="0%" />
-          <option value={10}></option>
-          <option value={20}></option>
-          <option value={30}></option>
-          <option value={40}></option>
-          <option value={50} label="50%" />
-          <option value={50}></option>
-          <option value={60}></option>
-          <option value={70}></option>
-          <option value={80}></option>
-          <option value={90}></option>
-          <option value={100}></option>
-        </datalist>
-        <span id="output">{this.values[this.state.currentStepIndex]}</span>
-      </div>
-    )
-  }
-}
-
-
-
