f27c26e9a698dcc1c6591d54fb451dae7f6c57b3
Jéssica Gonsalves <82064258+jessicagonsalves@users.noreply.github.com>
2021-10-05 17:37:58 -0300

    update navbar

--- containers/StepsMentoring/StepsMentoring.jsx
+++ containers/StepsMentoring/StepsMentoring.jsx
@@ -1,12 +1,9 @@
-<<<<<<< HEAD
 import React, {Fragment} from 'react'
 
 import {Heading} from '../../components/Heading'
 import {Section} from '../../components/Section'
 import {CardSequence} from '../../components/Card'
 import {Wave} from '../../components/Wave'
-
-import waveImage from '../../assets/image/wave-color.png'
 
 const content = {
   title:
@@ -30,35 +27,11 @@ const contentCards = [
   {
     title: 'REALIZE',
     text: 'a sua sessão de mentoria!',
-=======
-import Image from 'next/image'
-
-import {ItemsSection} from '../../components/ItemsSection'
-import {Section} from '../../components/Section'
-
-import studentsImage from '../../assets/image/loginImage2.png'
-
-const content = {
-  title: 'Como realizar a mentoria?',
-  journey: 'Passo a Passo',
-}
-
-const itemsText = [
-  {
-    text: 'Descubra quais são os seus maiores desafios',
-  },
-  {
-    text: 'Encontre seu mentor ideal com um clique',
-  },
-  {
-    text: 'Agende um horário e realize a mentoria!',
->>>>>>> parent of 9f5fc54 (update get started)
   },
 ]
 
 export default function StepsMentoring() {
   return (
-<<<<<<< HEAD
     <Fragment>
       <Wave 
         direction="down"
@@ -88,31 +61,5 @@ export default function StepsMentoring() {
         </div>
       </Section>
     </Fragment>
-=======
-      <div className="mentor-area" id="jornada-mentor">
-        <div className="content-area-2-l1">
-          <div className="container">
-            <div className="row justify-content-center align-items-center">
-              <ItemsSection itemsText={itemsText} title={content.title}>
-                {content.journey}
-              </ItemsSection>
-              <div className="offset-xxl-1 col-xxl-5 col-xl-6 col-lg-6 col-md-10">
-                <div
-                  className="content-2-l1-image-group"
-                  data-aos="fade-up"
-                  data-aos-delay={800}
-                  data-aos-duration={1000}
-                >
-                  <div className="image-1">
-                    <Image src={studentsImage} alt="image" />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
->>>>>>> parent of 9f5fc54 (update get started)
   )
 }
