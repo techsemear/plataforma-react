a88f93637e08c551b3ad7ede10d3f1be22ef612f
Jéssica Gonsalves <82064258+jessicagonsalves@users.noreply.github.com>
2021-10-06 19:39:30 -0300

    update stepinfos

--- components/AssessmentForm/StepInfos.jsx
+++ components/AssessmentForm/StepInfos.jsx
@@ -11,24 +11,23 @@ import {MultiSelectInput} from '../MultiSelectInput'
 import plataformaLogo from '../../assets/image/logo-semear.png'
 
 const macroArea = [
-  {area:'Administração '},
-  {area:'Artes: Visuais ou Digitais '},
-  {area:'Comunicação'},
-  {area:'Consultoria'},
-  {area:'Direito'},
-  {area:'Educação'},
-  {area:'Empreendedorismo'},
-  {area:'Engenharia '},
-  {area:'Finanças'},
-  {area:'Gestão '},
-  {area:'Marketing'},
-  {area:'Química'},
-  {area:'Saúde'},
-  {area:'Tecnologia'},
-  {area:'Vendas'},
-  {area:'Não Encontrei Minha Área'},
+  {area: 'Administração '},
+  {area: 'Artes: Visuais ou Digitais '},
+  {area: 'Comunicação'},
+  {area: 'Consultoria'},
+  {area: 'Direito'},
+  {area: 'Educação'},
+  {area: 'Empreendedorismo'},
+  {area: 'Engenharia '},
+  {area: 'Finanças'},
+  {area: 'Gestão '},
+  {area: 'Marketing'},
+  {area: 'Química'},
+  {area: 'Saúde'},
+  {area: 'Tecnologia'},
+  {area: 'Vendas'},
+  {area: 'Não Encontrei Minha Área'},
 ]
-
 
 const StepInfos = (props) => {
   if (props.currentStep !== 1) {
@@ -37,8 +36,8 @@ const StepInfos = (props) => {
 
   return (
     <fieldset>
-      <div className="plataforma-logo2">
-        <Image src={plataformaLogo} />
+      <div className="plataforma-logo2 mt-2">
+        <Image src={plataformaLogo} width={150} height={150}/>
       </div>
       <Heading
         level="h4"
@@ -46,14 +45,13 @@ const StepInfos = (props) => {
         ftColor="quinary"
         className="fs-title"
       >
-        Infos Profissionais 📋
+        Infos Acadêmicas e Profissionais 📋
       </Heading>
-      
 
       <Heading level="h6" className="label-picture">
         Nível de Escolaridade:
       </Heading>
-      
+
       <input
         list="datalist0"
         name="school"
@@ -77,94 +75,101 @@ const StepInfos = (props) => {
         <option value="Doutorado - Completo" />
       </datalist>
 
-      <Heading level="h6" className="label-picture">
-        Faculdade:
-      </Heading>
-      
-      <input
-        type="list"
-        list="datalist6"
-        name="faculdade"
-        placeholder=" Qual sua instituição de ensino?"
-        onChange={props.handleChange}
-        required
-      />
-      <datalist id="datalist6">
-        {UniversitiesList.map((item, index) => (
-          <option 
-            value={item.faculdade} 
-            key={index} 
+      <div className="row justify-content-center mt-2">
+        <div className="col-6">
+          <Heading level="h6" className="label-picture">
+            Faculdade:
+          </Heading>
+
+          <input
+            type="list"
+            list="datalist6"
+            name="faculdade"
+            placeholder=" Qual sua instituição de ensino?"
+            onChange={props.handleChange}
+            required
           />
-        ))}
-      </datalist>
-
-      <Heading level="h6" className="label-picture">
-        Curso:
-      </Heading>
-      
-
-      <input
-        type="list"
-        list="datalist4"
-        name="courses"
-        placeholder=" Qual seu curso?"
-        onChange={props.handleChange}
-        required
-      />
-      <datalist id="datalist4">
-        {CoursesList.map((item, index) => (
-          <option value={item.curso} key={index} />
-        ))}
-      </datalist>
-
-      <Heading level="h6" className="label-picture">
-        Área de Atuação:
-      </Heading>
-      
-      <input
-        type="list"
-        list="datalist5"
-        name="areas"
-        placeholder=" Qual sua principal area de atuação?"
-        onChange={props.handleChange}
-        required
-      />
-      <datalist id="datalist5"  multiple>
-        {macroArea.map((item, index) => (
-          <option value={item.area} key={index} />
-        ))}
-      </datalist>
-
-      <Heading level="h6" className="label-picture">
-        Subáreas de Atuação:
-      </Heading>
-      
-      <div className="multiSelectInput">
-        <MultiSelectInput/>
+          <datalist id="datalist6">
+            {UniversitiesList.map((item, index) => (
+              <option value={item.faculdade} key={index} />
+            ))}
+          </datalist>
+        </div>
+        <div className="col-6">
+          <Heading level="h6" className="label-picture">
+            Curso:
+          </Heading>
+          <input
+            type="list"
+            list="datalist4"
+            name="courses"
+            placeholder=" Qual seu curso?"
+            onChange={props.handleChange}
+            required
+          />
+          <datalist id="datalist4">
+            {CoursesList.map((item, index) => (
+              <option value={item.curso} key={index} />
+            ))}
+          </datalist>
+        </div>
       </div>
 
-      <Heading level="h6" className="label-picture">
-        Site Pessoal:
-      </Heading>
-      
-      <input
-        type="url"
-        name="site"
-        placeholder=" Você tem algum site pessoal ? (ex.: Linkedin ou Portfólio)"
-        value={props.site}
-        onChange={props.handleChange}
-        required
-      />
-      
+      <div className="row justify-content-center mt-2">
+        <div className="col-6">
+          <Heading level="h6" className="label-picture">
+            Área de Interesse:
+          </Heading>
 
-      <Heading level="h6" className="label-picture">
-        Gostaria de Adicionar uma Imagem ao seu perfil ?
-      </Heading>
-      <input type="file" accept="image/*" />
+          <input
+            type="list"
+            list="datalist5"
+            name="areas"
+            placeholder=" Qual sua principal area de interesse de forma macro?"
+            onChange={props.handleChange}
+            required
+          />
+          <datalist id="datalist5" multiple>
+            {macroArea.map((item, index) => (
+              <option value={item.area} key={index} />
+            ))}
+          </datalist>
+        </div>
+        <div className="col-6">
+          <Heading level="h6" className="label-picture">
+            Subáreas de Interesse:
+          </Heading>
 
+          <div className="multiSelectInput">
+            <MultiSelectInput />
+          </div>
+        </div>
+      </div>
+
+      <div className="row justify-content-center mt-2">
+        <div className="col-6">
+          <Heading level="h6" className="label-picture">
+            Site Pessoal:
+          </Heading>
+
+          <input
+            type="url"
+            name="site"
+            placeholder=" Você tem algum site pessoal ? (ex.: Linkedin ou Portfólio)"
+            value={props.site}
+            onChange={props.handleChange}
+            required
+          />
+        </div>
+        <div className="col-6">
+          <Heading level="h6" className="label-picture">
+            Gostaria de Adicionar uma Imagem ao seu perfil ?
+          </Heading>
+          <input type="file" accept="image/*" />
+        </div>
+      </div>
     </fieldset>
   )
 }
 
 export default StepInfos
-
