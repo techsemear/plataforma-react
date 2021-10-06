6528db0466e019d3b55e15a6bd6a96a33e1bddd7
Jéssica Gonsalves <82064258+jessicagonsalves@users.noreply.github.com>
2021-10-06 09:23:13 -0300

    Update NavbarGeneral.jsx

--- containers/NavbarMain/NavbarGeneral.jsx
+++ containers/NavbarMain/NavbarGeneral.jsx
@@ -4,20 +4,11 @@ import {Navbar} from '../../components/Navbar'
 
 import userIcon from '../../assets/image/landing-1/user-icon.png'
 
-const btnInfo = {
-  label: 'Perfil',
-  linkTo: '/login',
-}
-
 const menuItems = [
   {
     label: 'Perfil',
     linkTo: '/',
     image: userIcon,
-  },
-  {
-    label: ' ',
-    linkTo: '/',
   },
 ]
 
