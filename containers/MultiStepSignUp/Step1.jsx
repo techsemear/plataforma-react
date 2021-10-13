5e64dbf006a36d6b7de93b689bcf756599a812f0
Vinicius Ribeiro <vinicius.vrf@gmail.com>
2021-10-12 21:00:40 -0300

    ♻️ Refactor MultistepForm

--- containers/MultiStepSignUp/Step1.jsx
+++ containers/MultiStepSignUp/Step1.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react'
 import Image from 'next/image'
 
@@ -79,3 +80,82 @@ const Step1 = ({handleChange, fields}) => {
 }
 
 export default Step1
+||||||| parent of a613e23 (♻️ Refactor MultistepForm)
+=======
+import React from 'react'
+import Image from 'next/image'
+
+import {Heading} from '../../components/Heading'
+
+import plataformaLogo from '../../assets/image/logo-semear.png'
+
+const Step1 = ({handleChange, fields}) => {
+  return (
+    <fieldset>
+      <div className="plataforma-logo2">
+        <Image src={plataformaLogo} alt="" />
+      </div>
+
+      <Heading
+        level="h3"
+        horizontalPosition="center"
+        ftColor="quinary"
+        className="fs-title"
+      >
+        Crie sua Conta🌱
+      </Heading>
+
+      <Heading
+        level="h6"
+        horizontalPosition="center"
+        ftColor="quinary"
+        className="fs-subtitle"
+      >
+        Passo 1
+      </Heading>
+
+      <input
+        type="text"
+        name="email"
+        placeholder="Digite seu melhor email:"
+        value={fields.email}
+        onChange={handleChange}
+      />
+
+      <input
+        type="text"
+        name="name"
+        placeholder="Digite seu nome completo:"
+        value={fields.name}
+        onChange={handleChange}
+      />
+
+      <input
+        type="text"
+        name="username"
+        placeholder="Crie seu nome de usuário:"
+        value={fields.username}
+        onChange={handleChange}
+      />
+
+      <input
+        type="password"
+        name="password"
+        placeholder="Crie sua senha:"
+        value={fields.password}
+        onChange={handleChange}
+      />
+
+      <input
+        type="password"
+        name="passwordConfirmation"
+        placeholder="Confirme sua senha:"
+        value={fields.passwordConfirmation}
+        onChange={handleChange}
+      />
+    </fieldset>
+  )
+}
+
+export default Step1
+>>>>>>> a613e23 (♻️ Refactor MultistepForm)
