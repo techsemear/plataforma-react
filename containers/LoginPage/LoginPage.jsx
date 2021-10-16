f956391c6f94b10f34d10e2096e30987a35d3212
Vinicius Ribeiro <vinicius.vrf@gmail.com>
2021-10-16 19:33:49 -0300

    🐛 Fix bug

--- containers/LoginPage/LoginPage.jsx
+++ containers/LoginPage/LoginPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/dist/client/image'
+import Link from 'next/link'
 
 import {Heading} from '../../components/Heading'
 import {SlideShow} from '../../components/SlideShow'
@@ -11,7 +12,6 @@ import imageLogin1 from '../../assets/image/loginImage1.png'
 import imageLogin2 from '../../assets/image/loginImage2.png'
 import imageLogin3 from '../../assets/image/loginImage3.png'
 
-<<<<<<< HEAD
 const content = [
   {
     image: imageLogin1,
@@ -21,68 +21,12 @@ const content = [
   },
   {
     image: imageLogin3,
-  }
+  },
 ]
 
-
-
-export default function LoginPage(){
-||||||| parent of a613e23 (♻️ Refactor MultistepForm)
-
-
-export default function LoginPage(){
-=======
 export default function LoginPage() {
->>>>>>> a613e23 (♻️ Refactor MultistepForm)
   return (
-<<<<<<< HEAD
     <main className="login-main">
-    <div className="box">
-      <div className="inner-box">
-        <div className="forms-wrap">
-          <form action="index.html" autoComplete="off" className="sign-in-form">
-            <div className="plataforma-logo">
-              <Image
-                src = {plataformaLogo}
-                width={550}
-                height={550}
-              />
-            </div>
-            <div className="heading">
-              <Heading level="h2" horizontalPosition ="center" >Bem-vindo</Heading>
-              <Heading level= "h6" horizontalPosition ="center">Ainda não se registrou?  
-              <a href="/signUp" className="toggle"> Entre aqui</a> 
-              </Heading>
-            </div>
-            <div className="actual-form">
-              <div className="input-wrap">
-                <input type="text" minLength={4} className="input-field" autoComplete="off" required />
-                <label>Usuário</label>
-||||||| parent of a613e23 (♻️ Refactor MultistepForm)
-    <main>
-    <div className="box">
-      <div className="inner-box">
-        <div className="forms-wrap">
-          <form action="index.html" autoComplete="off" className="sign-in-form">
-            <div className="plataforma-logo">
-              <Image
-                src = {plataformaLogo}
-                width={550}
-                height={550}
-              />
-            </div>
-            <div className="heading">
-              <Heading level="h2">Bem-vindo</Heading>
-              <Heading level= "h6">Ainda não se registrou?  
-              <a href="/signUp" className="toggle"> Entre aqui</a> 
-              </Heading>
-            </div>
-            <div className="actual-form">
-              <div className="input-wrap">
-                <input type="text" minLength={4} className="input-field" autoComplete="off" required />
-                <label>Usuário</label>
-=======
-    <main>
       <div className="box">
         <div className="inner-box">
           <div className="forms-wrap">
@@ -93,105 +37,78 @@ export default function LoginPage() {
             >
               <div className="plataforma-logo">
                 <Image src={plataformaLogo} width={550} height={550} alt="" />
->>>>>>> a613e23 (♻️ Refactor MultistepForm)
-              </div>
-              <div className="heading">
-                <Heading level="h2">Bem-vindo</Heading>
-                <Heading level="h6">
-                  Ainda não se registrou?
-                  <a href="/signup" className="toggle">
-                    {' '}
-                    Entre aqui
-                  </a>
-                </Heading>
               </div>
               <div className="actual-form">
-                <div className="input-wrap">
-                  <input
-                    type="text"
-                    minLength={4}
-                    className="input-field"
-                    autoComplete="off"
-                    required
-                  />
-                  <label>Usuário</label>
-                </div>
-                <div className="input-wrap">
-                  <input
-                    type="password"
-                    minLength={4}
-                    className="input-field"
-                    autoComplete="off"
-                    required
-                  />
-                  <label>Senha</label>
-                </div>
-                <input
-                  type="submit"
-                  defaultValue="Sign In"
-                  className="sign-btn"
-                />
-                <div className="sign-with">
-                  <Heading
-                    level="h6"
-                    ftColor="quartenary"
-                    horizontalPosition="center"
-                    className="login-with"
-                  >
-                    Login With
+                <div className="heading">
+                  <Heading level="h2">Bem-vindo</Heading>
+                  <Heading level="h6">
+                    Ainda não se registrou?
+                    <a href="/signup" className="toggle">
+                      {' '}
+                      Entre aqui
+                    </a>
                   </Heading>
-                  <div className="sign-with-btn">
-                    <a href="#" className="btn-face">
-                      <Image src={facebookLogo} alt="FACEBOOK" />
-                      Facebook
-                    </a>
-                    <a href="#" className="btn-google">
-                      <Image src={googleLogo} alt="GOOGLE" />
-                      Google
-                    </a>
-                  </div>
                 </div>
-                <p className="text">
-                  Esqueceu sua senha?
-                  <a href="#"> Obtenha ajuda</a>
-                </p>
+                <div className="actual-form">
+                  <div className="input-wrap">
+                    <input
+                      type="text"
+                      minLength={4}
+                      className="input-field"
+                      autoComplete="off"
+                      required
+                    />
+                    <label>Usuário</label>
+                  </div>
+                  <div className="input-wrap">
+                    <input
+                      type="password"
+                      minLength={4}
+                      className="input-field"
+                      autoComplete="off"
+                      required
+                    />
+                    <label>Senha</label>
+                  </div>
+                  <input
+                    type="submit"
+                    defaultValue="Sign In"
+                    className="sign-btn"
+                  />
+                  <div className="sign-with">
+                    <Heading
+                      level="h6"
+                      ftColor="quartenary"
+                      horizontalPosition="center"
+                      className="login-with"
+                    >
+                      Login With
+                    </Heading>
+                    <div className="sign-with-btn">
+                      <a href="#" className="btn-face">
+                        <Image src={facebookLogo} alt="FACEBOOK" />
+                        Facebook
+                      </a>
+                      <a href="#" className="btn-google">
+                        <Image src={googleLogo} alt="GOOGLE" />
+                        Google
+                      </a>
+                    </div>
+                  </div>
+                  <p className="text">
+                    Esqueceu sua senha?
+                    <a href="#"> Obtenha ajuda</a>
+                  </p>
+                </div>
               </div>
-<<<<<<< HEAD
-              <p className="text">
-                Esqueceu sua senha?
-                <a href="#"> Obtenha ajuda</a>
-              </p>
-            </div>
-          </form>
-        </div>
-        <div className="slider">
-          <SlideShow
-           duration={500}
-           transitionDuration	={500}
-           contentInfo={content}
-          />
-          <Heading className="slider-text" level="h3" ftColor = "default" horizontalPosition="center" >Faça parte dessa jornada !</Heading>
-        </div>
-||||||| parent of a613e23 (♻️ Refactor MultistepForm)
-              <p className="text">
-                Esqueceu sua senha?
-                <a href="#"> Obtenha ajuda</a>
-              </p>
-            </div>
-          </form>
-        </div>
-        <div className="slider">
-          <SlideShow
-           duration={500}
-           transitionDuration	={500}
-          />
-          <Heading className="slider-text" level="h3" ftColor = "default" horizontalPosition="center" >Faça parte dessa jornada !</Heading>
-        </div>
-=======
             </form>
           </div>
           <div className="slider">
-            <SlideShow duration={500} transitionDuration={500} />
+            <SlideShow
+              duration={500}
+              transitionDuration={500}
+              contentInfo={content}
+            />
             <Heading
               className="slider-text"
               level="h3"
@@ -201,7 +118,6 @@ export default function LoginPage() {
               Faça parte dessa jornada !
             </Heading>
           </div>
->>>>>>> a613e23 (♻️ Refactor MultistepForm)
         </div>
       </div>
     </main>
