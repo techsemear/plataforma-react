1ecb4ebcf4db79053db89925041b1a32b51bdd00
Jéssica Gonsalves <82064258+jessicagonsalves@users.noreply.github.com>
2021-10-20 19:07:58 -0300

    Update HeaderNavigation.jsx

--- components/HeaderNavigation/HeaderNavigation.jsx
+++ components/HeaderNavigation/HeaderNavigation.jsx
@@ -2,7 +2,6 @@ import Link from 'next/link'
 import Image from 'next/image'
 
 import {HeaderItems} from './HeaderItems'
-import {Logo} from '../Logo'
 
 import semearLogo from '../../assets/image/semear/logo-light.png'
 
