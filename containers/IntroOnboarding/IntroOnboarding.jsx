6832f8a7c542fb1a1ee577b315439dfe1f794bb7
Jéssica Gonsalves <82064258+jessicagonsalves@users.noreply.github.com>
2021-10-23 18:32:29 -0300

    Reactoring IntroOnboarding

--- containers/IntroOnboarding/IntroOnboarding.jsx
+++ containers/IntroOnboarding/IntroOnboarding.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import {Fragment} from 'react'
 import Image from 'next/image'
 
-import {Button} from '../../components/Button'
+import styles from './IntroOnboarding.module.css'
+
 import {Wave} from '../../components/Wave'
 import {Heading} from '../../components/Heading'
 
@@ -12,7 +12,7 @@ import headerImage from '../../assets/image/onboarding-header.png'
 
 export default function IntroOnboarding(){
     return (
-      <div className="hero-area-l6 ">
+      <div className={styles.onboardingArea}>
         <div className="container">
           <div className="row justify-content-center">
             <div
@@ -21,19 +21,19 @@ export default function IntroOnboarding(){
               data-aos-delay={500}
               data-aos-duration={1000}
             >
-              <div className="hero-l6-content text-center">
+              <div className={`${styles.onboardingContent} text-center`}>
                 <Heading
                   level="h2"
                   ftColor="default"
                   horizontalPosition="center"
                 >
-                  MIRE NO SEU BRILHANTE FUTURO !
+                  MIRE NO SEU BRILHANTE FUTURO!
                 </Heading>
                 <Heading
                   level="h5"
                   ftColor="default"
                   horizontalPosition="center"
-                  className="text-paragraph"
+                  className={styles.textParagraph}
                 >
                   <br />O caminho para a independência e conforto financeiro é
                   árduo e o vestibular foi só o primeiro passo. Vamos percorrer
@@ -48,15 +48,15 @@ export default function IntroOnboarding(){
               data-aos-delay={700}
               data-aos-duration={1000}
             >
-              <div className="hero-l6-content-image">
+              <div className={styles.onboardingContentImage}>
                 <Image className="w-100" src={headerImage} alt="image" />
               </div>
             </div>
           </div>
         </div>
-        <Image src={waveImage} className="wave-image" />
+        <Image src={waveImage} className={styles.waveImage} alt="waveImage" />
         <Wave
-          className="wave introWave"
+          className={`wave ${styles.introWave}`}
           direction="up"
           color="#f4fcfb"
           bgColor="#11115d"
