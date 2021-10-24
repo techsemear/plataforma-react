94a7beb7f28bdfdf1edef88532100075346eb23e
Jéssica Gonsalves <82064258+jessicagonsalves@users.noreply.github.com>
2021-10-24 12:50:39 -0300

    Minor Fixes

--- components/Card/CardItems.jsx
+++ components/Card/CardItems.jsx
@@ -2,7 +2,6 @@ import React, {Fragment} from 'react'
 
 import {Heading} from '../Heading'
 
-
 export default function CardItems({contentInfo = [], reverse = true}) {
   return (
     <Fragment>
@@ -28,15 +27,12 @@ export default function CardItems({contentInfo = [], reverse = true}) {
   )
 }
 
-function ContentCard({item, index}) {
+function ContentCard({item}) {
   return (
     <div className="feature-box-l2 h-100">
       <div className="d-flex">
         <div
-          className={[
-            'color-box',
-            item.color ? item.color : 'bg-secondBlue',
-          ].join(' ')}
+          className={`color-box ${item.color ? item.color : 'bg-secondBlue'}`}
         />
         <div className="content-box">
           <Heading level="h4" ftColor="primary">
